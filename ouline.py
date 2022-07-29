2b811c6f0d769b487765a1e55c8ff24abfcdbd95
mariamconde <condemriam@gmail.com>
2022-07-29 00:36:24 -0400

    My first change

---
+++ ouline.py
@@ -0,0 +1,25 @@
+#import beautifulsoup and request here
+# functions
+
+def displayJobDetails():
+    print("Display job details")
+
+#function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
+def getJobList(role,location):
+    url = 'https://www.indeed.com/jobs?q={role}&l={location}'
+    # Complete the missing part of this function here 
+
+#save data in JSON file
+def saveDataInJSON(jobDetails):
+    #Complete the missing part of this function here
+    print("Saving data to JSON")
+
+#main function
+def main():
+    # Write a code here to get job location and role from user e.g. role = input()
+    print("Enter role you want to search")
+    role = input()
+    # Complete the missing part of this function here
+    
+if __name__ == '__main__':
+    main()
