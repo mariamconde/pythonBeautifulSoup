5fcedc22d07641188f59eb099a23d7b8a39c98f6
mariamconde <condemriam@gmail.com>
2022-08-03 22:06:12 -0400

    main function complete

--- ouline.py
+++ ouline.py
@@ -1,29 +1,44 @@
-#import beautifulsoup and request here
+# import beautifulsoup and request here
 import requests
+import json
 
-# functions
 
-def displayJobDetails():
-    print("Display job details")
+# function to get job list from url 'https://www.monster.com/jobs/search?q={role}&where={location}'
+def getJobList(role, location):
+    url = 'https://www.monster.com/jobs/search?q={role}&where={location}'
+    # Complete the missing part of this function here
 
-#function to get job list from url 'https://www.indeed.com/jobs?q={role}&l={location}'
-def getJobList(role,location):
-    url = 'https://www.indeed.com/jobs?q={role}&l={location}'
-    # Complete the missing part of this function here 
+# save data in JSON file
 
-#save data in JSON file
+
 def saveDataInJSON(jobDetails):
-    #Complete the missing part of this function here
+    # Complete the missing part of this function here
     print("Saving data to JSON")
 
-#main function
+# main function
+
+
 def main():
     # Write a code here to get job location and role from user e.g. role = input()
     print("Enter role you want to search")
     role = input()
     # Complete the missing part of this function here
-    
+    print("Enter location: ")
+    location = input()
+    print("Job role: ", role)
+    print("location: ", location)
+    print("\n\nResults:\n\n")
+    jobs = getJobList(role, location)
+
+    # print jobs and details
+    for job in jobs:
+        for detail in job:
+            print(detail)
+        print("\n\n")
+
+    data = {"Title": "test"}
+    saveDataInJSON(data)
+
+
 if __name__ == '__main__':
     main()
-
-response = requests.request("GET", url, headers=headers, data=payload)
